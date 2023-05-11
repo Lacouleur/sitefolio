d85c0744f6e88e82134c1a4e18724d5f83ae0279
Damien Voindrot <damien.voindrot@link-value.fr>
2023-05-11 16:13:04 +0200

    Updates

--- src/utils/helpers/navigationHelpers.js
+++
@@ -1,8 +0,0 @@
-export function handleClickScroll(name) {
-  const element = document.getElementById(name);
-  const yOffset = name === "paintings" ? -170 : -120;
-  const y = element.getBoundingClientRect().top + window.pageYOffset + yOffset;
-  if (element) {
-    window.scrollTo({ top: y, behavior: "smooth" });
-  }
-}
